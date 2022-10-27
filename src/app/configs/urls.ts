16a4cde5619bc9c84683fdef73b931e9d38fedbd
PPRomaa <popadinets.roman1@gmail.com>
2022-10-28 00:05:59 +0300

    lesson 1

---
+++ src/app/configs/urls.ts
@@ -0,0 +1,10 @@
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+
+const urls = {
+  users: `${baseURL}/users`,
+  posts: `${baseURL}/posts`,
+  comments: `${baseURL}/comments`
+}
+
+export {urls}
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {urls} from "../configs";
+import {IComment} from "../interfaces/comment.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAll(): Observable<IComment[]> {
+    return this.httpClient.get<IComment[]>(urls.comments)
+  }
+}
